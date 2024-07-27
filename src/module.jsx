85342a9a4c8a883c0188dcae677f7df3ff82fa31
Lotlin <lotlin22@gmail.com>
2024-07-27 19:37:27 +0200

    start

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import img from './img/book-768@2x.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img src={img} />
+  </>
+)
+
+export default Module;
